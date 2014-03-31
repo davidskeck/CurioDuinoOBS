e9b07683d019039960bd3531463330aca5255e16
davidskeck <dstevenkeck@gmail.com>
2014-03-30 21:33:35 -0500

    Removed all references to compass libraries and object. Compass was just not accurate enough.

--- CurioDuinoOBS/CurioDuinoOBS.ino
+++ CurioDuinoOBS/CurioDuinoOBS.ino
@@ -18,31 +18,19 @@
 #include <QTRSensors.h>
 #include <ZumoMotors.h>
 
+// Pre-package libraries
 #include <ZumoBuzzer.h>
-#include <Wire.h>
-#include <LSM303.h>
 #include <NilRTOS.h>
-#include <NilRTOS.h>
-
-// Use tiny unbuffered NilRTOS NilSerial library.
 #include <NilSerial.h>
+
+// Custom libraries
+#include <CurioDuinoData.h>
+#include <CurioDuinoNav.h>
 
 // Macro to redefine Serial as NilSerial to save RAM.
 // Remove definition to use standard Arduino Serial.
 #define Serial NilSerial
 
-#include <CurioDuinoData.h>
-#include <CurioDuinoNav.h>
-
-// Compass
-#define CALIBRATION_SAMPLES 70  // Number of compass readings to take when calibrating
-#define CRB_REG_M_2_5GAUSS 0x60 // CRB_REG_M value for magnetometer +/-2.5 gauss full scale
-#define CRA_REG_M_220HZ    0x1C // CRA_REG_M value for magnetometer 220 Hz update rate
-
-// Allowed deviation (in degrees) relative to target angle that must be achieved before driving straight
-#define DEVIATION_THRESHOLD 5
-
-LSM303 compass;
 ZumoBuzzer buzzer;
 CurioDuinoData data;
 CurioDuinoNav nav;
@@ -62,64 +50,12 @@ void checkDataAndWait()
   }
 }
 
-void calibrateCompass()
-{
-  // The highest and lowest possible magnetic value to read in any direction is 2047 or -2047
-  LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32767, -32767, -32767};
-  
-  unsigned char index;
-  
-  // To calibrate the magnetometer, the Zumo spins to find the max/min
-  // magnetic vectors. This information is used to correct for offsets
-  // in the magnetometer data.
-  nav.motors.setLeftSpeed(FORWARD_SPEED);
-  nav.motors.setRightSpeed(-FORWARD_SPEED);
-
-  for(index = 0; index < CALIBRATION_SAMPLES; index ++)
-  {
-    // Take a reading of the magnetic vector and store it in compass.m
-    compass.read();
-
-    running_min.x = min(running_min.x, compass.m.x);
-    running_min.y = min(running_min.y, compass.m.y);
-
-    running_max.x = max(running_max.x, compass.m.x);
-    running_max.y = max(running_max.y, compass.m.y);
-    
-    delay(50);
-  }
-  
-  nav.stopMoving();
-  
-  // Set calibrated values to compass.m_max and compass.m_min
-  compass.m_max.x = running_max.x;
-  compass.m_max.y = running_max.y;
-  compass.m_min.x = running_min.x;
-  compass.m_min.y = running_min.y;
-  
-  // Reset to accomodate calibrate compass button in GUI
-  data.isStarted = false;
-}
-
 void setup()
 {  
   // Open serial port at 9600 baud
   Serial.begin(9600);
-  
-  // Initiate the Wire library and join the I2C bus as a master
-  Wire.begin();
 
-  // Initiate LSM303
-  compass.init();
-
-  // Enables accelerometer and magnetometer
-  compass.enableDefault();
-
-  compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 gauss sensitivity to hopefully avoid overflow problems
-  compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ);    // 220 Hz compass update rate
-
-  checkDataAndWait();
-  calibrateCompass();
+  // Wait for GUI to signal start
   checkDataAndWait();
 }
 
